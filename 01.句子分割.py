864b31f03320d0c0df34767cbddb6b06c7a4013b
fancey.zou <1404248584@qq.com>
2026-01-27 11:51:21 +0800

    feat: first commit project base code

---
+++ 01.句子分割.py
@@ -0,0 +1,16 @@
+# import re
+
+text = "现在AI已经很卷了，大家都可以学习了吗？我下现在才开罅隙店皮毛，呢哈哈哈"
+# sent = re.split(r'(.｜?|!|\...)', text)
+# print(sent)  # ['现在AI已经很卷了，大家都可以学习了吗？', '我下现在才开罅隙店皮毛，呢哈哈哈']
+
+
+# 递归分割
+# def overlap_split(text, n, stride):
+#     for i in range(0, len(text), stride):
+#         print(i, i + n)
+#         print(text[i:i + n])
+
+
+# overlap_split(text, 10, 5)
+
