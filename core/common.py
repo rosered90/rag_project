1f1fdb943d69f42f4e15ed2d236d7c708bc88027
fancey.zou <1404248584@qq.com>
2026-01-30 15:22:56 +0800

    feat: import langchain

--- core/common.py
+++ core/common.py
@@ -1,4 +1,5 @@
 import os
+from urllib import response
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import (
@@ -8,29 +9,39 @@ from langchain_core.prompts import (
 from openai import OpenAI
 
 
-llm = ChatOpenAI(
-    model_name="qwen-max-latest",
-    # openai_api_key=SecretStr(os.getenv("DASHSCOPE_API_KEY")),
-    openai_api_key=os.getenv("DASHSCOPE_API_KEY"),
-    openai_api_base="https://dashscope.aliyuncs.com/compatible-mode/v1",
-    temperature=0,
-    streaming=True,
-)
 
-system_message_template = ChatMessagePromptTemplate.from_template(
-    template="你是一个{role}的专家，擅长回答{domain}领域的问题。", role="system"
-)
-human_message_template = ChatMessagePromptTemplate.from_template(
-    template="用户问题：{question}", role="user"
-)
-chat_prompt_template = ChatPromptTemplate.from_messages(
-    [
-        system_message_template,
-        human_message_template,
-    ]
-)
+# llm = ChatOpenAI(
+#     model_name="qwen-max-latest",
+#     # openai_api_key=SecretStr(os.getenv("DASHSCOPE_API_KEY")),
+#     openai_api_key=os.getenv("DASHSCOPE_API_KEY"),
+#     openai_api_base="https://dashscope.aliyuncs.com/compatible-mode/v1",
+#     temperature=0,
+#     streaming=True,
+# )
+
+# system_message_template = ChatMessagePromptTemplate.from_template(
+#     template="你是一个{role}的专家，擅长回答{domain}领域的问题。", role="system"
+# )
+# human_message_template = ChatMessagePromptTemplate.from_template(
+#     template="用户问题：{question}", role="user"
+# )
+# chat_prompt_template = ChatPromptTemplate.from_messages(
+#     [
+#         system_message_template,
+#         human_message_template,
+#     ]
+# )
 
 
 # vector embedding client
 load_dotenv()
 rag_client = OpenAI(api_key=os.getenv("api_key"), base_url=os.getenv("base_url"))
+
+# langchain llm client
+llm = ChatOpenAI(
+    model_name="qwen-turbo",
+    openai_api_key=os.getenv("api_key"),
+    openai_api_base=os.getenv("base_url"),
+    temperature=0,
+    streaming=True,
+)
